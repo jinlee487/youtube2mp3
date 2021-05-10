ae571707d4c23b764a11f3d6b9590d5764b32b3d
jayjay <jinwoolee131@gmail.com>
2021-05-11 00:52:43 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+
+
 # youtube2mp3
-custom youtube video to mp3 converter
+custom Youtube video to mp3 converter
+### 👋 Introduction 
+
+Hello World! 
+
+I am working on recording videos and needed audio files from Youtube videos.
+I could use Youtube to mp3 conversion service sites... but I am a programmer. 
+So I went ahead and created a simple script to download Youtube audios straight from Youtube.
+
+### 📑 How to Install and Run
+
+The installation requirements for this python script is 
+- python 3
+- pafy
+- youtube-dl
+
+Once you have python 3 downloaded, follow the instructions below.
+
+1. Clone the repo in your desired location.
+2. Run the Powershell commands in the run.txt in order.
+3. Download the video.
+
+# Things to Note 
+- The videos will be downloaded to your repo location only. pafy only allows downloads in your current path where the script is being run from.
+- If you are using virtual env, remember to activate and deactivate the env.
+## References
+- `downloading virtual env` : [https://python-guide-kr.readthedocs.io/ko/latest/dev/virtualenvs.html](https://python-guide-kr.readthedocs.io/ko/latest/dev/virtualenvs.html)
+- `using pafy` : [https://www.geeksforgeeks.org/youtube-mediaaudio-download-using-python-pafy/]( https://www.geeksforgeeks.org/youtube-mediaaudio-download-using-python-pafy/)
+
+
+Author: <a href="https://github.com/jinlee487">JWL</a>
---
+++ youtube2mp3.py
@@ -0,0 +1,49 @@
+
+#https://python-guide-kr.readthedocs.io/ko/latest/dev/virtualenvs.html
+#https://www.geeksforgeeks.org/youtube-mediaaudio-download-using-python-pafy/
+import pafy
+import os
+
+def run():
+    url = input("\ninput youtube video link below.\n")
+    print("\nrequested url:"  +url)
+    video= pafy.new(url)
+    # print title
+    print("title: " + video.title)
+    # print rating
+    # print(video.rating)
+    # print viewcount
+    # print(video.viewcount)
+    # print author & video length
+    print("account: ", video.author)
+    print("length: ", video.length)
+    # print duration, likes, dislikes & description
+    # print(video.duration, video.likes, video.dislikes, video.description)
+    audiostreams = video.audiostreams
+    count = 0
+    for i in audiostreams: 
+        print('index: %s, bitrate: %s, ext: %s, size:%0.2fMb' % (count,i.bitrate, i.extension, i.get_filesize()/1024/1024))
+        count+=1
+    index = int(input("\nchoose the file you want to download\n"))
+    print("you chose index: %s\n bitrate: %s, ext: %s, size:%0.2fMb" % (index, audiostreams[index].bitrate, audiostreams[index].extension, audiostreams[index].get_filesize()/1024/1024))
+    choice = input("\n작업을 계속 진행하고 싶으시면 Y 아니면 y 를 입력해주세요.\n\n").strip()
+    if choice == "Y" or choice == "y":
+        try:
+            audiostreams[index].download()
+            # bestaudio = video.getbestaudio()
+            # bestaudio.download()
+            cwd = os.getcwd()
+            print("\n  다음 경로로"+cwd+"\n\n  "+video.title+"\n\n  파일을 성공적으로 저장하였습니다.\n")
+            print("\nOperation successful.....Terminating the program.\n")
+        except Exception as e:
+            print("\n\nAn exception occurred!\n")
+            print(e)
+            print("\n\t.........Terminating the program.\n\n\n")
+    else:
+        print("\n.....Terminating the program.\n")
+    quit()
+
+run()
+
+
+
